e9d9a3c1035a5aedcccc930c3260c3f45059361d
Ravi Hebbar <ravihebbar6@gmail.com>
2020-02-18 13:21:20 +0530

    Completed lesson 7

--- app/router.js
+++ app/router.js
@@ -7,19 +7,20 @@ const Router = EmberRouter.extend({
 });
 
 Router.map(function() {
-
   this.route('about');
   this.route('contact');
 
   this.route('admin', function() {
     this.route('invitations');
     this.route('contacts');
+    this.route('seeder');
   });
-
   this.route('libraries', function() {
     this.route('new');
     this.route('edit', { path: '/:library_id/edit' });
   });
+  this.route('authors');
+  this.route('books');
 });
 
 export default Router;
